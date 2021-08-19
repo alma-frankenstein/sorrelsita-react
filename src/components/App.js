017f52af0b3e912d3f50f083ea74d40b3437d5f9
Alma <a.q.frankenstein@gmail.com>
2021-08-19 15:23:12 -0700

    create outline for app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../../src/App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <h4>in the App.js file</h4>
+    </React.Fragment>
+  );
+}
+
+export default App;
