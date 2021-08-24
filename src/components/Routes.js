d35fdcd6463e1427151ff31aa468443dff6a3e82
Alma <a.q.frankenstein@gmail.com>
2021-08-24 12:16:10 -0700

    mock up routes for router

---
+++ src/components/Routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HomeContent from "./Home";
+import Content from "./Content";
+import ContactInfo from "./Contact";
+import Navbar from "./Navbar/Navbar";
+import Route from 'react-router-dom';
+import Switch from 'react-router-dom';
+import Redirect from 'react-router-dom';
+
+function Routes(){
+  return(
+    <div>
+      <Navbar />
+      <Switch>
+        <Route exact path="/home" component={HomeContent} />
+        <Route exact path="/">
+          <Redirect to="/home" />
+        </Route>
+        <Route exact path="/about" component={Content} />
+        <Route exact path="/contact" component={ContactInfo} />
+      </Switch>
+    </div>
+  );
+}
+
+export default Routes;
