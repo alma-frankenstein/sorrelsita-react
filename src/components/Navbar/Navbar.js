be05f8511ba8adbbcf07a3a1d963ad81c85b18f8
Alma <a.q.frankenstein@gmail.com>
2021-08-24 13:25:04 -0700

    simplify nav link, delete MenuItems

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Nav } from "reactstrap";
-import MenuItems from "./MenuItems";
 import "./Navbar.css";
 
 class Navbar extends React.Component {
@@ -10,20 +9,14 @@ class Navbar extends React.Component {
           <h1>Welcome</h1>
 
           <div class="topnav">
-          {/* // <nav className="NavbarItems"> */}
-          {/* <h1 className="navbar-logo">navbar logo</h1> */}
           {/* <img src= "./favicon.ico" alt="favicon"/> */}
           <img src= "./furgus.jpg" alt="furgus" height="50px"/>
 
           <div className="menu-icon">
           </div>
-              {MenuItems.map((item, index) => {
-                return (
-                  <a key={index} className={item.cName} href={item.url}>
-                  {item.label}
-                </a>
-                )
-              })}
+            <a href="/home">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
         </div>
       </div>
     )
