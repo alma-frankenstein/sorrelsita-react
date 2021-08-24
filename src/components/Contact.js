555999ea2f5bfcd6b80b3dbd40feb74a073f5796
Alma <a.q.frankenstein@gmail.com>
2021-08-24 11:32:45 -0700

    add outline of contact page

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactInfo(){
+  return(
+    <React.Fragment>
+      <p>in the Contact.js file</p>
+    </React.Fragment>
+  )
+}
+
+export default ContactInfo;
