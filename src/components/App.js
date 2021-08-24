82306b190d32cc319a88d4d1bef1af7bd696a267
Alma <a.q.frankenstein@gmail.com>
2021-08-24 14:41:55 -0700

    get bootstrap carousel example working just in Home component

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,41 @@ import React from 'react';
 import Content from "./Content";
 import HomeContent from "./Home";
 import Navbar from "./Navbar/Navbar";
+// import Carousel from 'react-bootstrap/Carousel';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <React.Fragment>
+{/* 
+<div style={{ display: 'block', width: 700, padding: 30 }}>
+      <h4>React-Bootstrap Carousel Component</h4>
+      <Carousel>
+        <Carousel.Item interval={1500}>
+          <img
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+            alt="Image One"
+          />
+          <Carousel.Caption>
+            <h3>Label for first slide</h3>
+            <p>Sample Text for Image One</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+            alt="Image Two"
+          />
+          <Carousel.Caption>
+            <h3>Label for second slide</h3>
+            <p>Sample Text for Image Two</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div> */}
+
       <h4>in the App.js file</h4>
       <Navbar />
     </React.Fragment>
