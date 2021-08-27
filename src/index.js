5a38761fc1155c4b7ff6dabcd1ff74dba0c57bcb
Alma <a.q.frankenstein@gmail.com>
2021-08-27 10:42:11 -0700

    move import of custom styling to after bootstrap import in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Routes from './components/Routes';
 import { BrowserRouter } from 'react-router-dom';
 
