f587a70f394f37623bc7f61be7809c171e7fc9b3
Alma <a.q.frankenstein@gmail.com>
2021-08-30 12:28:34 -0700

    placeholder data and pictures on pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,24 @@ import React from "react";
 function ContactInfo(){
   return(
     <React.Fragment>
-      <p>in the Contact.js file</p>
+      <div class="row">
+      <div class="col-7">
+        <h3>Contact Sorrel:</h3>
+        <p>Phone: xxx-xxx-xxxx</p>
+        <p>Email: Sorrel@Sorrel.com</p>
+        <p>Singing Telegram: xxx</p>
+      </div>
+
+      {/* <div class="row"> */}
+      <div class="col-5">
+        <img src="img/IMG-3152.JPG" 
+        className="d-block w-50"
+        alt="placeholder picture"
+        style={{marginTop: 10}} />
+      </div>
+      </div>
+      {/* </div> */}
+
     </React.Fragment>
   )
 }
