07a0424bc3e13d484eb957dba5568b7065c95355
Alma <a.q.frankenstein@gmail.com>
2021-08-30 12:14:58 -0700

    add some placeholder content info

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,8 +3,10 @@ import React from "react";
 function Content(){
   return (
     <React.Fragment>
-      <p>content from Content.js</p>
-      {/* <img src="../../public/img/furgus.jpg" alt="furgie pic"/> */}
+      <p>Here's some stuff about how great Sorrel is, how much
+        everyone likes her, how thoughtful and artistic she is,
+        and how you should totally hire her.</p>
+      <p>(plus pictures!)</p>
       <img src="./furgus.jpg" alt="furgie pic" width="100px"/>
 
     </React.Fragment>
